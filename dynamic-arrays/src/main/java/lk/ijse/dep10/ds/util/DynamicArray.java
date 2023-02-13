f482212402309e88d54f9c5e481aba54e1fa3604
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2023-02-13 17:45:50 +0530

    Add linked-list

--- dynamic-arrays/src/main/java/lk/ijse/dep10/ds/util/DynamicArray.java
+++ dynamic-arrays/src/main/java/lk/ijse/dep10/ds/util/DynamicArray.java
@@ -1,25 +1,60 @@
 package lk.ijse.dep10.ds.util;
 
+import java.util.Arrays;
+
 public class DynamicArray {
 
+    private int[] data = new int[0];
+
     public void add(String value) {
+        int[] temp = new int[data.length + 1];
+
+        for (int i = 0; i < data.length; i++) {
+            temp[i] = data[i];
+        }
+
+        temp[temp.length - 1] = Integer.parseInt(value);
+
+        data = temp;
     }
 
     public void clear() {
+        data = new int[0];
     }
 
     public int size() {
-        return 5;
+        return data.length;
     }
 
     public String get(int index) {
-        return "10";
+        return data[index] + "";
     }
 
     public boolean contains(String input) {
+        for (int num : data) {
+            if (num == Integer.parseInt(input)) return true;
+        }
         return false;
     }
 
     public void remove(int index) {
+        if (data.length == 0) return;
+
+        int[] temp = new int[data.length - 1];
+
+        for (int i = 0; i < data.length; i++) {
+            if (i < index){
+                temp[i] = data[i];
+            }else if (i > index){
+                temp[i - 1] = data[i];
+            }
+        }
+
+        data = temp;
+    }
+
+    @Override
+    public String toString() {
+        return Arrays.toString(data);
     }
 }
---
+++ linked-list/src/main/java/lk/ijse/dep10/ds/util/SinglyLinkedList.java
@@ -0,0 +1,65 @@
+package lk.ijse.dep10.ds.util;
+
+public class SinglyLinkedList {
+    private Node head;
+    private Node tail;
+    private int size;
+
+    public void add(String value) {
+        Node node = new Node(Integer.parseInt(value), null);
+        if (size == 0) {
+            head = node;
+            tail = head;
+        } else {
+            tail.next = node;
+            tail = node;
+        }
+        size++;
+    }
+
+    @Override
+    public String toString() {
+        String data = "[";
+
+        Node temp = head;
+        while (temp != null) {
+            data += temp.data + ", ";
+            temp = temp.next;
+        }
+        if (size != 0) data += "\b";
+        data += "]";
+        return data;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public void clear() {
+        head = null;
+        tail = head;
+        size = 0;
+    }
+
+    public String get(int index) {
+        return "10";
+    }
+
+    public boolean contains(String input) {
+        return false;
+    }
+
+    public void remove(int index) {
+        size--;
+    }
+}
+
+class Node {
+    int data;
+    Node next;
+
+    public Node(int data, Node next) {
+        this.data = data;
+        this.next = next;
+    }
+}
